32b54cecdb9db37fb66ffbb037bb10a772eb2c35
IthronRandir <frtopo@yandex.by>
2023-04-20 16:09:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp50/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void LeapYear(int year)
+        {
+            string yearStr = Convert.ToString(year);
+
+            DateTime date1 = new DateTime(year, 01, 01);
+
+            year++;
+
+            DateTime date2 = new DateTime(year, 01, 01);
+
+            int DaysInYear = (date2-date1).Days;
+            bool b = DaysInYear == 366;
+
+            Console.WriteLine(b);
+        }
+        static void Main(string[] args)
+        {
+            int year = 2024;
+            LeapYear(year);
+        }
+    }
+}
